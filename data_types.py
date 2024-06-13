056519266895f1c80e7721b1a4217bb0fe394b87
rayney2 <masonsuess@gmail.com>
2024-06-13 14:03:03 +0800

    Added fields to creatures data data_types Set up apply_attack and added abilities data type

--- data_types.py
+++ data_types.py
@@ -34,8 +34,10 @@ class Creature:
 
     damage = [0, 0]
     hp = 0
-    attack = 0
-    defense = 0
+    melee_attack = 0
+    ranged_attack = 0
+    melee_defense = 0
+    ranged_defense = 0     #Added ranged and melee subdivison
     move = 0
     speed = 0
     shots = 0
@@ -43,3 +45,36 @@ class Creature:
     spells = []
     spell_power = 0
     mana = 0
+    luck = 0              #Added morale and luck
+    morale = 0
+
+
+#Ability class split up into two definition, conditional abilities like the pirates or flying
+#On attack abilities like curse
+#How to implement abilities like charge
+@dataclass
+class Abilities:
+    name = ""
+    description = ""
+    data = None
+    def apply(self, attacker, defender):
+        """Default apply method."""
+        pass
+
+class ConditionalAbility(Abilities):
+    def check_condition(self, creature, context):
+        """Check specific conditions"""
+        return False
+    
+    def apply_condition(self, creature, context):
+        """Apply effects based on condition"""
+        pass
+
+class CombatAbility(Abilities):
+    def apply_on_attack(self, attacker, defender):
+        """Apply during attack"""
+        pass
+
+
+ 
+
